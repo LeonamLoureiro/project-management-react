952a2cebeb4a9b4a79642e07717b64fd813c2960
LeonamLoureiro <leonamfed@gmail.com>
2023-01-26 14:44:33 -0300

    navbar

--- costs/src/App.js
+++ costs/src/App.js
@@ -3,6 +3,7 @@ import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar';
@@ -18,7 +19,8 @@ function App() {
           <Route path= "/" element={<Home/>}/> 
           <Route path= "/Company" element={<Company/>}/> 
           <Route path= "/Contact" element={<Contact/>}/> 
-          <Route path= "/NewProject" element={<NewProject/>}/>    
+          <Route path= "/NewProject" element={<NewProject/>}/>
+          <Route path= "/Projects" element={<Projects/>}/>
         </Routes>
       </Container>
 
--- costs/src/components/layout/Navbar.js
+++ costs/src/components/layout/Navbar.js
@@ -1,15 +1,29 @@
 import {Link} from 'react-router-dom'
+import Container from './Container'
+import styles from './Navbar.module.css'
+import logo from '../../img/costs_logo.png'
 
 function Navbar() {
     return (
-        <div>
-            <ul>
-                <Link to="/">Home</Link>
-                <Link to="/Company">Company</Link>
-                <Link to="/Contact">Contact</Link>
-                <Link to="/NewProject">New Project</Link>
-            </ul>
-        </div>
+        <nav className={styles.navbar}>
+            <Container>
+                <Link to="/"><img src={logo} alt="Logo"/></Link>
+                <ul className={styles.list}>
+                    <li className={styles.item}>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className={styles.item}>    
+                        <Link to="/Company">Company</Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to="/Contact">Contact</Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to="/Projects">Projects</Link>
+                    </li>
+                </ul>
+            </Container>
+        </nav>
     )
 }
 
