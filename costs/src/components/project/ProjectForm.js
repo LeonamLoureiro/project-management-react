8613a1242c5450c393c78d24973e1fa189af380c
LeonamLoureiro <leonamfed@gmail.com>
2023-01-30 21:06:59 -0300

    adjust new project page

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -8,21 +8,22 @@ function ProjectForm() {
 
     const [categories, setCategories] = useState([])
 
-    
-    fetch('http://localhost:5000/categories', {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    }).then((resp) => resp.json()).then((data) => {setCategories(data)}).catch((err) => console.log(err))
+    useEffect(() => { 
+        fetch('http://localhost:5000/categories', {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((resp) => resp.json()).then((data) => {setCategories(data)}).catch((err) => console.log(err))
+    }, [])
 
     return (
         <div>
             <form className={styles.form}>
                 <Input type="text" text="Name of Project" name="name" placeholder="Insert project name"/>
                 <Input type="number" text="Project Budget" name="budget" placeholder="Insert project total budget"/>
-                <Select name="category_id" text="Select a Category" />                    
-                <Submit text="Create Project" options={categories}/>
+                <Select name="category_id" text="Select a Category" options={categories} />                    
+                <Submit text="Create Project" />
             </form>
         </div>
     )
